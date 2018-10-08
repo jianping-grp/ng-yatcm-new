4c36ed77def5d15889a9ac7597f2d2a0d769e2fc
machunfeng <493036599@qq.com>
2018-10-08 23:39:53 +0800

    delete cytoscape

--- src/app/setting.ts
+++ src/app/setting.ts
@@ -1,5 +1,5 @@
 export const REST_HOST = 'http://192.168.0.104:9000/api';
-
+// 'http://222.30.50.66/yatcm_api'
 // default pagination size
 export let PER_PAGE = 10;
 export let PER_PAGE_OPT = [5, 10, 50, 100];
